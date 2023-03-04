eac7037e8d69dfc91b39d38fa3308d3bf4355133
yuyinws <lyc657508366@gmail.com>
2023-03-04 13:42:55 +0800

    refactor(server): remove axios

--- server/core/utils/index.ts
+++ server/core/utils/index.ts
@@ -1,14 +1,21 @@
 import { getImage } from '../request/steamApi'
 
 export const imageUrl2Base64 = async (url: string): Promise<string> => {
-  const image = await getImage(url)
-  if (image) {
-    const _base64 = Buffer.from(image).toString('base64')
-    return _base64
+  try {
+    const image = await getImage(url)
+    if (image) {
+      const _base64 = Buffer.from(image).toString('base64')
+      return _base64
+    }
+    return ''
   }
-  return ''
+  catch (error) {
+    return ''
+  }
 }
 
 export const string2Boolean = (value: string) => {
   return value === 'true'
 }
+
+export const transparentImageBase64 = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
