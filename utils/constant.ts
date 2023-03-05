3b4551642fdd49e644d241f3256e22dfea342a9b
yuyinws <lyc657508366@gmail.com>
2023-03-05 16:13:45 +0800

    refactor: components

---
+++ utils/constant.ts
@@ -0,0 +1,20 @@
+export const themeList = ['dark', 'light', 'radical', 'tokyonight', 'solarized-light', 'ocean-dark']
+export const languages = [
+  {
+    label: '简体中文',
+    value: 'zh-CN',
+  },
+  {
+    label: 'English',
+    value: 'en',
+  },
+]
+export const statisticsList = [
+  'groups',
+  'badges',
+  'games',
+  'screenshots',
+  'guides',
+  'artworks',
+  'reviews',
+]
