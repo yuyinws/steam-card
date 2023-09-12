8dc1529a5ca0d6e13064604fa7aede240c330932
yuyinws <lyc657508366@gmail.com>
2023-09-12 15:25:56 +0800

    chore: deps update

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-const steamcardUrl = ref('')
-const isImgLoading = ref(true)
+
 </script>
 
 <template>
-  <div flex="~ wrap" items-start justify-center gap-20px>
-    <ConfigPanel
-      v-model:loading="isImgLoading"
-      v-model:url="steamcardUrl"
-    />
-    <Preview
-      v-model:loading="isImgLoading"
-      :steamcard-url="steamcardUrl"
-    />
+  <div>
+    <UButton>11</UButton>
   </div>
 </template>
-
-<style>
-.i-switch {
-  justify-content: flex-start!important;
-  gap: 10px;
-}
-</style>
