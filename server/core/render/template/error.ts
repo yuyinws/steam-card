3d206aa219034d4a1636d0d63749151371447c36
yuyinws <lyc657508366@gmail.com>
2024-03-11 17:27:24 +0800

    chore: clean code

--- server/core/render/template/error.ts
+++ server/core/render/template/error.ts
@@ -1,5 +1,5 @@
 export function generateError(errMsg: string, errInfo: string) {
-  return `
+  return /* svg */`
     <svg width="400" height="140" xmlns="http://www.w3.org/2000/svg">
       <rect fill="#F3F4F6" rx="4.5" stroke="#e4e2e2" stroke-opacity="1" width="100%" height="100%"></rect>
       <text x="10" y="50" text-anchor="start" fill="red">
